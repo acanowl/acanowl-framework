56fbec47a32608dccbd714bce431df6d253fa988
徐仔 <kami.x1g@163.com>
2025-01-09 18:39:11 +0800

    🧪 test: 调整测试用例

--- packages/utils/test/object/index.spec.ts
+++ packages/utils/test/object/index.spec.ts
@@ -1,64 +1,67 @@
 import { expect, test } from 'vitest'
 import { deepClone, deepMerge, isArray } from '../../dist'
 
-const objectValue1 = {
-  a: 1,
-  b: { c: 2 },
-  d: [1, 2, 5],
-  e: 'hello'
-}
-const objectValue2 = {
-  b: { f: 3 },
-  d: [1, 4],
-  e: 'world',
-  g: true
-}
+test('deepClone objectValue1', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  expect(deepClone(objectValue)).toStrictEqual({ a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' })
+})
 
-const cloneValue1 = deepClone(objectValue1)
+test('cloneValue1 === objectValue1', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  const cloneValue = deepClone(objectValue)
+  expect(cloneValue === objectValue).toBe(false)
+})
 
-test('deepClone objectValue1', () =>
-  expect(deepClone(objectValue1)).toStrictEqual({
-    a: 1,
-    b: { c: 2 },
-    d: [1, 2, 5],
-    e: 'hello'
-  }))
-test('cloneValue1 === objectValue1', () => expect(cloneValue1 === objectValue1).toBe(false))
-test('cloneValue1.b === objectValue1.b', () => expect(cloneValue1.b === objectValue1.b).toBe(false))
+test('cloneValue1.b === objectValue1.b', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  const cloneValue = deepClone(objectValue)
+  expect(cloneValue.b === objectValue.b).toBe(false)
+})
 
-test('deepMerge objectValue2', () =>
-  expect(deepMerge(objectValue1, objectValue2)).toStrictEqual({
+test('deepMerge objectValue2', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  const objectValue2 = { b: { f: 3 }, d: [1, 4], e: 'world', g: true }
+  expect(deepMerge(objectValue, objectValue2)).toStrictEqual({
     a: 1,
     b: { c: 2, f: 3 },
     d: [1, 4],
     e: 'world',
     g: true
-  }))
+  })
+})
 
-const customizer = (targetValue: unknown, sourceValue: unknown) => {
-  if (isArray(targetValue) && isArray(sourceValue)) {
-    return targetValue.concat(sourceValue)
+test('deepMerge objectValue2 concat', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  const objectValue2 = { b: { f: 3 }, d: [1, 4], e: 'world', g: true }
+
+  const customizer = (targetValue: unknown, sourceValue: unknown) => {
+    if (isArray(targetValue) && isArray(sourceValue)) {
+      return targetValue.concat(sourceValue)
+    }
   }
-}
-test('deepMerge objectValue2 concat', () =>
-  expect(deepMerge(objectValue1, objectValue2, customizer)).toStrictEqual({
+  expect(deepMerge(objectValue, objectValue2, customizer)).toStrictEqual({
     a: 1,
     b: { c: 2, f: 3 },
     d: [1, 2, 5, 1, 4],
     e: 'world',
     g: true
-  }))
+  })
+})
 
-const setCustomizer = (targetValue: unknown, sourceValue: unknown) => {
-  if (isArray(targetValue) && isArray(sourceValue)) {
-    return [...new Set([...targetValue, ...sourceValue])]
+test('deepMerge objectValue2 setCustomizer', () => {
+  const objectValue = { a: 1, b: { c: 2 }, d: [1, 2, 5], e: 'hello' }
+  const objectValue2 = { b: { f: 3 }, d: [1, 4], e: 'world', g: true }
+
+  const setCustomizer = (targetValue: unknown, sourceValue: unknown) => {
+    if (isArray(targetValue) && isArray(sourceValue)) {
+      return [...new Set([...targetValue, ...sourceValue])]
+    }
   }
-}
-test('deepMerge objectValue2 setCustomizer', () =>
-  expect(deepMerge(objectValue1, objectValue2, setCustomizer)).toStrictEqual({
+  expect(deepMerge(objectValue, objectValue2, setCustomizer)).toStrictEqual({
     a: 1,
     b: { c: 2, f: 3 },
     d: [1, 2, 5, 4],
     e: 'world',
     g: true
-  }))
+  })
+})
--- packages/utils/test/valid/index.spec.ts
+++ packages/utils/test/valid/index.spec.ts
@@ -1,67 +1,143 @@
 import { expect, test } from 'vitest'
 
-const emptyObject = {}
-const validObject = { a: 1 }
-const emptyArray = []
-const validArray = [1]
-const string = 'string'
-const number = 1
-const boolean = true
-const date = new Date()
-const regExp = /regExp/
-const func = function () {}
-const asyncfunc = async function () {}
-const nullValue = null
-const undefinedValue = undefined
-
 import { isPlainObject } from '../../dist'
-test('isPlainObject emptyObject, 返回 true', () => expect(isPlainObject(emptyObject)).toBe(true))
-test('isPlainObject validObject, 返回 true', () => expect(isPlainObject(validObject)).toBe(true))
-test('isPlainObject validArray, 返回 false', () => expect(isPlainObject(validArray)).toBe(false))
+test('isPlainObject emptyObject, 返回 true', () => {
+  const emptyObject = {}
+  expect(isPlainObject(emptyObject)).toBe(true)
+})
+test('isPlainObject validObject, 返回 true', () => {
+  const validObject = { a: 1 }
+  expect(isPlainObject(validObject)).toBe(true)
+})
+test('isPlainObject validArray, 返回 false', () => {
+  const validArray = [1]
+  expect(isPlainObject(validArray)).toBe(false)
+})
 
 import { isObject } from '../../dist'
-test('isObject emptyObject, 返回 true', () => expect(isObject(emptyObject)).toBe(true))
-test('isObject validObject, 返回 true', () => expect(isObject(validObject)).toBe(true))
-test('isObject validArray, 返回 true', () => expect(isObject(validArray)).toBe(true))
-test('isObject date, 返回 false', () => expect(isObject(date)).toBe(false))
-test('isObject nullValue, 返回 false', () => expect(isObject(nullValue)).toBe(false))
+test('isObject emptyObject, 返回 true', () => {
+  const emptyObject = {}
+  expect(isObject(emptyObject)).toBe(true)
+})
+test('isObject validObject, 返回 true', () => {
+  const validObject = { a: 1 }
+  expect(isObject(validObject)).toBe(true)
+})
+test('isObject validArray, 返回 true', () => {
+  const validArray = [1]
+  expect(isObject(validArray)).toBe(true)
+})
+test('isObject date, 返回 false', () => {
+  const date = new Date()
+  expect(isObject(date)).toBe(false)
+})
+test('isObject nullValue, 返回 false', () => {
+  const nullValue = null
+  expect(isObject(nullValue)).toBe(false)
+})
 
 import { isArray } from '../../dist'
-test('isArray emptyArray, 返回 true', () => expect(isArray(emptyArray)).toBe(true))
-test('isArray validArray, 返回 true', () => expect(isArray(validArray)).toBe(true))
-test('isArray string, 返回 false', () => expect(isArray(string)).toBe(false))
+test('isArray emptyArray, 返回 true', () => {
+  const emptyArray = []
+  expect(isArray(emptyArray)).toBe(true)
+})
+test('isArray validArray, 返回 true', () => {
+  const validArray = [1]
+  expect(isArray(validArray)).toBe(true)
+})
+test('isArray string, 返回 false', () => {
+  const string = 'string'
+  expect(isArray(string)).toBe(false)
+})
 
 import { isString } from '../../dist'
-test('isString string, 返回 true', () => expect(isString(string)).toBe(true))
-test('isString number, 返回 false', () => expect(isString(number)).toBe(false))
+test('isString string, 返回 true', () => {
+  const string = 'string'
+  expect(isString(string)).toBe(true)
+})
+test('isString number, 返回 false', () => {
+  const number = 1
+  expect(isString(number)).toBe(false)
+})
 
 import { isNumber } from '../../dist'
-test('isNumber number, 返回 true', () => expect(isNumber(number)).toBe(true))
-test('isNumber string, 返回 false', () => expect(isNumber(string)).toBe(false))
+test('isNumber number, 返回 true', () => {
+  const number = 1
+  expect(isNumber(number)).toBe(true)
+})
+test('isNumber string, 返回 false', () => {
+  const string = 'string'
+  expect(isNumber(string)).toBe(false)
+})
 
 import { isBoolean } from '../../dist'
-test('isBoolean boolean, 返回 true', () => expect(isBoolean(boolean)).toBe(true))
-test('isBoolean string, 返回 false', () => expect(isBoolean(string)).toBe(false))
+test('isBoolean boolean, 返回 true', () => {
+  const boolean = true
+  expect(isBoolean(boolean)).toBe(true)
+})
+test('isBoolean string, 返回 false', () => {
+  const string = 'string'
+  expect(isBoolean(string)).toBe(false)
+})
 
 import { isFunction } from '../../dist'
-test('isFunction func, 返回 true', () => expect(isFunction(func)).toBe(true))
-test('isFunction asyncfunc, 返回 true', () => expect(isFunction(asyncfunc)).toBe(true))
-test('isFunction nullValue, 返回 false', () => expect(isFunction(nullValue)).toBe(false))
+test('isFunction func, 返回 true', () => {
+  const func = function () {}
+  expect(isFunction(func)).toBe(true)
+})
+test('isFunction asyncfunc, 返回 true', () => {
+  const asyncfunc = async function () {}
+  expect(isFunction(asyncfunc)).toBe(true)
+})
+test('isFunction nullValue, 返回 false', () => {
+  const nullValue = null
+  expect(isFunction(nullValue)).toBe(false)
+})
 
 import { isDate } from '../../dist'
-test('isDate date, 返回 true', () => expect(isDate(date)).toBe(true))
-test('isDate string, 返回 false', () => expect(isDate(string)).toBe(false))
+test('isDate date, 返回 true', () => {
+  const date = new Date()
+  expect(isDate(date)).toBe(true)
+})
+test('isDate string, 返回 false', () => {
+  const string = 'string'
+  expect(isDate(string)).toBe(false)
+})
 
 import { isRegExp } from '../../dist'
-test('isRegExp regExp, 返回 true', () => expect(isRegExp(regExp)).toBe(true))
-test('isRegExp string, 返回 false', () => expect(isRegExp(string)).toBe(false))
+test('isRegExp regExp, 返回 true', () => {
+  const regExp = /regExp/
+  expect(isRegExp(regExp)).toBe(true)
+})
+test('isRegExp string, 返回 false', () => {
+  const string = 'string'
+  expect(isRegExp(string)).toBe(false)
+})
 
 import { isDefined } from '../../dist'
-test('isDefined regExp, 返回 true', () => expect(isDefined(regExp)).toBe(true))
-test('isDefined string, 返回 true', () => expect(isDefined(string)).toBe(true))
-test('isDefined nullValue, 返回 false', () => expect(isDefined(nullValue)).toBe(false))
-test('isDefined undefinedValue, 返回 false', () => expect(isDefined(undefinedValue)).toBe(false))
+test('isDefined regExp, 返回 true', () => {
+  const regExp = /regExp/
+  expect(isDefined(regExp)).toBe(true)
+})
+test('isDefined string, 返回 true', () => {
+  const string = 'string'
+  expect(isDefined(string)).toBe(true)
+})
+test('isDefined nullValue, 返回 false', () => {
+  const nullValue = null
+  expect(isDefined(nullValue)).toBe(false)
+})
+test('isDefined undefinedValue, 返回 false', () => {
+  const undefinedValue = undefined
+  expect(isDefined(undefinedValue)).toBe(false)
+})
 
 import { isNull } from '../../dist'
-test('isNull nullValue, 返回 true', () => expect(isNull(nullValue)).toBe(true))
-test('isNull undefinedValue, 返回 false', () => expect(isNull(undefinedValue)).toBe(false))
+test('isNull nullValue, 返回 true', () => {
+  const nullValue = null
+  expect(isNull(nullValue)).toBe(true)
+})
+test('isNull undefinedValue, 返回 false', () => {
+  const undefinedValue = undefined
+  expect(isNull(undefinedValue)).toBe(false)
+})
